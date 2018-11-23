71617586d9407239f908d2ce4a5333d4a5797b9e
Nikolay Kurin <nikolay.kurin@progforce.com>
2018-11-23 21:23:29 +0300

    remove unnecessaary

--- r_scripts/build.R
+++ r_scripts/build.R
@@ -4,12 +4,6 @@ setwd("/tmp")
 
 data <- read.table("results.data", sep=",", head=TRUE)
 
-data <- na.omit(data)
-data <- data[order(data$treatment_duration),]
-
-ind <- sample(2, nrow(data), replace=TRUE, prob=c(0.9, 0.1))
-train <- data[ind==1,]
-
-reg <- nls(treatment_duration ~ patient_age*a + patient_gender*b + treatment_complexity*c + treatment_phases_count*d, data=train, start=c(a=0,b=0,c=0,d=0))
+reg <- nls(treatment_duration ~ patient_age*a + patient_gender*b + treatment_complexity*c + treatment_phases_count*d, data=data, start=c(a=0,b=0,c=0,d=0))
 
 save(reg, file = "nonlinear_regression_model.rda")
